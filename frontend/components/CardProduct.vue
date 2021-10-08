2cad6ac74ba1c9faaf1fe8daf41da497ccb1f4df
Calvinputra <89188672+Calvinputra@users.noreply.github.com>
2021-10-09 00:06:17 +0700

    component card

--- frontend/components/CardProduct.vue
+++ frontend/components/CardProduct.vue
@@ -1,7 +1,11 @@
 <template>
   <section class="container">
     <div class="card rounded shadow mb-5 bg-white rounded" style="width: 18rem">
-      <img src="img/audiblelogo.png" class="card-img-top" alt="..." />
+      <img
+        src="img/audiblelogo.png"
+        class="card-img-top height:auto"
+        alt="..."
+      />
       <div class="card-body">
         <div class="row">
           <h5 class="col-sm-9 card-title">Nama Produk</h5>
@@ -9,19 +13,27 @@
             ><img src="img/audiblelogo.png" alt=""
           /></a>
         </div>
-        <p class="card-text">Rp.120.000</p>
+        <p class="card-text mb-4 font-weight-bold">Rp.120.000</p>
         <div class="row">
           <a
             href="#"
-            class="btn text-danger shadow rounded col-sm-6 me-2"
+            class="btn text-danger btn-sm shadow rounded col-sm-5 ms-2 pt-2"
             style="background-color: #f3f3f3"
             >+ Keranjang</a
           >
-          <a class="col-sm-2 me-2" style="background-color: #f3f3f3" href=""
-            ><img class="img-fluid" src="img/Whatapps.png" alt=""
+          <a class="col-sm-3" href=""
+            ><img
+              class="img-fluid max-width:100% height:auto rounded"
+              style="background-color: #f3f3f3"
+              src="img/Whatsapp_new.png"
+              alt=""
           /></a>
-          <a class="col-sm-2" style="background-color: #f3f3f3" href=""
-            ><img src="img/audiblelogo.png" alt=""
+          <a class="col-sm-3" href=""
+            ><img
+              class="img-fluid max-width:100% height:auto rounded"
+              style="background-color: #f3f3f3"
+              src="img/tokopedia.png"
+              alt=""
           /></a>
         </div>
       </div>
