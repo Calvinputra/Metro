0aa87f00b80e2bebadffa8f85b0026425a05b581
Jonathan <jonathanchang96@gmail.com>
2021-10-08 16:26:09 +0700

    install adminbro

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,3 +1,5 @@
+const URL = "http://localhost:3000";
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -39,7 +41,10 @@ export default {
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    proxy: true,
+    baseURL: URL
+  },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
