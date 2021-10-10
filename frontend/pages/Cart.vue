61c249ae3f0469125b0440960520c54dc7b2cb7b
Calvinputra <89188672+Calvinputra@users.noreply.github.com>
2021-10-10 22:28:09 +0700

    beranda

--- frontend/pages/Cart.vue
+++ frontend/pages/Cart.vue
@@ -5,10 +5,14 @@
       <!-- Alert-->
       <nav class="container my-4" aria-label="breadcrumb">
         <ol class="py-1 breadcrumb">
-          <li class="my-2 ms-3 breadcrumb-item">
-            <a style="text-decoration: none" href="/">Beranda</a>
+          <li class="my-2 ms-3 breadcrumb-item opacity-50">
+            <a style="text-decoration: none; color: black" href="/">Beranda</a>
           </li>
-          <li class="my-2 breadcrumb-item active" aria-current="page">
+          <li
+            class="my-2 breadcrumb-item active opacity-50"
+            style="color: black"
+            aria-current="page"
+          >
             Keranjang Saya
           </li>
         </ol>
