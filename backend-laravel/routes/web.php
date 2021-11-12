1f8a68a108d0e26dc6dd0be86bd778eafa3ce948
Jonathan <jonathanchang96@gmail.com>
2021-11-12 15:48:27 +0700

    setting send email

--- backend-laravel/routes/web.php
+++ backend-laravel/routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,9 +21,21 @@ Route::group(['prefix' => ''], function () {
     Voyager::routes();
 });
 Route::group(['prefix' => 'debug'], function () {
-    Route::get('/check_user',function(){
+    Route::get('/check_user', function () {
         $user_data = \App\Models\User::all();
         //dd(password_hash('Sycar12345!@#$%',PASSWORD_DEFAULT));
         $verify = password_verify('Sycar12345!@#$%', '$2y$10$6qTN5MnXqQCidcAk9nn4qOIkM/mCE5nP0OOn2ZspNq/wzPhMHn6BC');
     });
+});
+
+Route::get('send-mail', function () {
+    //https://www.itsolutionstuff.com/post/laravel-8-mail-laravel-8-send-email-tutorialexample.html
+    $details = [
+        'title' => 'Mail from ItSolutionStuff.com',
+        'body'  => 'This is for testing email using smtp',
+    ];
+
+    \Mail::to('jonathanchang96@gmail.com')->send(new \App\Mail\MyTestMail($details));
+
+    dd("Email is Sent.");
 });
