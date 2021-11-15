387f51e2799ca87635f5fe4cb244cf85e7017112
jasonrenata <54830225+jasonrenata@users.noreply.github.com>
2021-11-16 00:11:59 +0700

    AboutUs Struktur

---
+++ frontend/pages/AboutUs.vue
@@ -0,0 +1,58 @@
+<template>
+  <section>
+    <Header />
+    <Breadcrumb :links="breadcrumb" />
+    <section>
+      <div class="ml-5 mr-5 pl-5 pr-5">
+        <h4 class="text-center">Metro Jaya</h4>
+        <p class="ml-5 mr-5 pl-5 pr-5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet
+          dui non metus aliquet laoreet in mollis lorem. Interdum et malesuada
+          fames ac ante ipsum primis in faucibus. Proin sollicitudin, dolor ut
+          aliquet euismod, erat libero congue dolor, ac tempus dolor erat vel
+          quam. Nulla tincidunt augue tincidunt, aliquet nisi eget, mattis
+          velit. Mauris tincidunt, justo ac eleifend faucibus, risus lacus porta
+          odio, eu iaculis enim orci a arcu. Ut mollis, lorem a finibus
+          convallis, nunc nisl congue orci, eu viverra elit libero vel massa.
+          Cras sit amet nibh et mauris luctus condimentum. Sed quis eleifend
+          lacus.
+        </p>
+      </div>
+      <br />
+      <div class="text-center">
+        <h4 class="text-center">Road Map</h4>
+        <img class="pb-5" src="/img/metro.png" alt="" />
+        <br />
+
+        <h4 class="text-center">Kelebihan Metro Jaya</h4>
+      </div>
+      <div>
+        <img class="w-25 p-5" src="/img/metro.png" alt="" />
+        <img class="w-25 p-5" src="/img/metro.png" alt="" />
+        <img class="w-25 p-5" src="/img/metro.png" alt="" />
+        <img class="w-25 p-5" src="/img/metro.png" alt="" />
+      </div>
+      <div class="mr-5 ml-5 pr-5 pl-5">
+        <p class="mr-5 ml-5 pr-5 pl-5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet
+          dui non metus aliquet laoreet in mollis lorem. Interdum et malesuada
+          fames ac ante ipsum primis in faucibus. Proin sollicitudin, dolor ut
+          aliquet euismod, erat libero congue dolor, ac tempus dolor erat vel
+          quam. Nulla tincidunt augue tincidunt, aliquet nisi eget, mattis
+          velit. Mauris tincidunt, justo ac eleifend faucibus, risus lacus porta
+          odio, eu iaculis enim orci a arcu. Ut mollis, lorem a finibus
+          convallis, nunc nisl congue orci, eu viverra elit libero vel massa.
+          Cras sit amet nibh et mauris luctus condimentum. Sed quis eleifend
+          lacus.
+        </p>
+      </div>
+      <br />
+      <div class="text-center">
+        <h4>Lokasi Kami</h4>
+        <img src="/img/metro.png" alt="" />
+      </div>
+    </section>
+
+    <Footer />
+  </section>
+</template>
