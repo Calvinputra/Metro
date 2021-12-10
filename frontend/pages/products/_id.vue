a7cf5736f9a3e5986bc0b6281d8f61828842725c
Jonathan <jonathanchang96@gmail.com>
2021-12-10 16:01:47 +0700

    detail product example

--- frontend/pages/products/_id.vue
+++ frontend/pages/products/_id.vue
@@ -5,57 +5,55 @@
       <Breadcrumb :links="breadcrumb" />
       <div class="container">
         <div class="row">
-          <div class="col-sm-1"></div>
-          <div class="col-sm-4">
-            <div
-              class="ecommerce-gallery"
-              data-mdb-zoom-effect="true"
-              data-mdb-auto-height="true"
-            >
-              <div class="row py-3 shadow-5">
-                <div class="col-12 mb-1">
-                  <div class="lightbox">
-                    <img
-                      :src="ASSET_URL + '/' + JSON.parse(data.images)[0]"
-                      alt="Gallery image 1"
-                      class="ecommerce-gallery-main-img active w-100"
-                    />
-                  </div>
-                </div>
-                <div class="col-3 mt-1">
-                  <img
-                    src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14a.jpg"
-                    data-mdb-img="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14a.jpg"
-                    alt="Gallery image 1"
-                    class="active w-100"
-                  />
-                </div>
-                <div class="col-3 mt-1">
-                  <img
-                    src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg"
-                    data-mdb-img="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg"
-                    alt="Gallery image 2"
-                    class="w-100"
-                  />
-                </div>
-                <div class="col-3 mt-1">
-                  <img
-                    src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg"
-                    data-mdb-img="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg"
-                    alt="Gallery image 3"
-                    class="w-100"
-                  />
-                </div>
-                <div class="col-3 mt-1">
-                  <img
-                    src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15a.jpg"
-                    data-mdb-img="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15a.jpg"
-                    alt="Gallery image 4"
-                    class="w-100"
-                  />
-                </div>
+          <div class="preview col-md-6">
+            <div class="preview-pic tab-content">
+              <div class="tab-pane active" id="pic-1">
+                <img
+                  :src="ASSET_URL + '/' + JSON.parse(data.images)[0]"
+                  alt="Gallery image 1"
+                  class="ecommerce-gallery-main-img active w-100"
+                />
+              </div>
+              <div class="tab-pane" id="pic-2">
+                <img :src="ASSET_URL + '/' + JSON.parse(data.images)[1]" />
+              </div>
+              <div class="tab-pane" id="pic-3">
+                <img src="http://placekitten.com/400/252" />
+              </div>
+              <div class="tab-pane" id="pic-4">
+                <img src="http://placekitten.com/400/252" />
+              </div>
+              <div class="tab-pane" id="pic-5">
+                <img src="http://placekitten.com/400/252" />
               </div>
             </div>
+            <ul class="preview-thumbnail nav nav-tabs">
+              <li class="active">
+                <a data-target="#pic-1" data-toggle="tab"
+                  ><img :src="ASSET_URL + '/' + JSON.parse(data.images)[0]"
+                /></a>
+              </li>
+              <li>
+                <a data-target="#pic-2" data-toggle="tab"
+                  ><img :src="ASSET_URL + '/' + JSON.parse(data.images)[1]"
+                /></a>
+              </li>
+              <li>
+                <a data-target="#pic-3" data-toggle="tab"
+                  ><img src="http://placekitten.com/200/126"
+                /></a>
+              </li>
+              <li>
+                <a data-target="#pic-4" data-toggle="tab"
+                  ><img src="http://placekitten.com/200/126"
+                /></a>
+              </li>
+              <li>
+                <a data-target="#pic-5" data-toggle="tab"
+                  ><img src="http://placekitten.com/200/126"
+                /></a>
+              </li>
+            </ul>
           </div>
           <div class="col-sm-6 mt-5 container">
             <h5>Nama Merek</h5>
@@ -229,6 +227,27 @@
       <Ulasan />
     </section>
     <Footer />
+
+    <!-- JQuery -->
+    <script
+      type="text/javascript"
+      src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+    ></script>
+    <!-- Bootstrap tooltips -->
+    <script
+      type="text/javascript"
+      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"
+    ></script>
+    <!-- Bootstrap core JavaScript -->
+    <script
+      type="text/javascript"
+      src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"
+    ></script>
+    <!-- MDB core JavaScript -->
+    <script
+      type="text/javascript"
+      src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"
+    ></script>
   </section>
 </template>
 
@@ -273,4 +292,221 @@ export default {
   },
 };
 </script>
+
+<style scoped lang="css">
+body {
+  font-family: "open sans";
+  overflow-x: hidden;
+}
+
+img {
+  max-width: 100%;
+}
+
+.preview {
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -webkit-flex-direction: column;
+  -ms-flex-direction: column;
+  flex-direction: column;
+}
+@media screen and (max-width: 996px) {
+  .preview {
+    margin-bottom: 20px;
+  }
+}
+
+.preview-pic {
+  -webkit-box-flex: 1;
+  -webkit-flex-grow: 1;
+  -ms-flex-positive: 1;
+  flex-grow: 1;
+}
+
+.preview-thumbnail.nav-tabs {
+  border: none;
+  margin-top: 15px;
+}
+.preview-thumbnail.nav-tabs li {
+  width: 18%;
+  margin-right: 2.5%;
+}
+.preview-thumbnail.nav-tabs li img {
+  max-width: 100%;
+  display: block;
+}
+.preview-thumbnail.nav-tabs li a {
+  padding: 0;
+  margin: 0;
+}
+.preview-thumbnail.nav-tabs li:last-of-type {
+  margin-right: 0;
+}
+
+.tab-content {
+  overflow: hidden;
+}
+.tab-content img {
+  width: 100%;
+  -webkit-animation-name: opacity;
+  animation-name: opacity;
+  -webkit-animation-duration: 0.3s;
+  animation-duration: 0.3s;
+}
+
+.card {
+  margin-top: 50px;
+  background: #eee;
+  padding: 3em;
+  line-height: 1.5em;
+}
+
+@media screen and (min-width: 997px) {
+  .wrapper {
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+  }
+}
+
+.details {
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -webkit-flex-direction: column;
+  -ms-flex-direction: column;
+  flex-direction: column;
+}
+
+.colors {
+  -webkit-box-flex: 1;
+  -webkit-flex-grow: 1;
+  -ms-flex-positive: 1;
+  flex-grow: 1;
+}
+
+.product-title,
+.price,
+.sizes,
+.colors {
+  text-transform: UPPERCASE;
+  font-weight: bold;
+}
+
+.checked,
+.price span {
+  color: #ff9f1a;
+}
+
+.product-title,
+.rating,
+.product-description,
+.price,
+.vote,
+.sizes {
+  margin-bottom: 15px;
+}
+
+.product-title {
+  margin-top: 0;
+}
+
+.size {
+  margin-right: 10px;
+}
+.size:first-of-type {
+  margin-left: 40px;
+}
+
+.color {
+  display: inline-block;
+  vertical-align: middle;
+  margin-right: 10px;
+  height: 2em;
+  width: 2em;
+  border-radius: 2px;
+}
+.color:first-of-type {
+  margin-left: 20px;
+}
+
+.add-to-cart,
+.like {
+  background: #ff9f1a;
+  padding: 1.2em 1.5em;
+  border: none;
+  text-transform: UPPERCASE;
+  font-weight: bold;
+  color: #fff;
+  -webkit-transition: background 0.3s ease;
+  transition: background 0.3s ease;
+}
+.add-to-cart:hover,
+.like:hover {
+  background: #b36800;
+  color: #fff;
+}
+
+.not-available {
+  text-align: center;
+  line-height: 2em;
+}
+.not-available:before {
+  font-family: fontawesome;
+  content: "\f00d";
+  color: #fff;
+}
+
+.orange {
+  background: #ff9f1a;
+}
+
+.green {
+  background: #85ad00;
+}
+
+.blue {
+  background: #0076ad;
+}
+
+.tooltip-inner {
+  padding: 1.3em;
+}
+
+@-webkit-keyframes opacity {
+  0% {
+    opacity: 0;
+    -webkit-transform: scale(3);
+    transform: scale(3);
+  }
+  100% {
+    opacity: 1;
+    -webkit-transform: scale(1);
+    transform: scale(1);
+  }
+}
+
+@keyframes opacity {
+  0% {
+    opacity: 0;
+    -webkit-transform: scale(3);
+    transform: scale(3);
+  }
+  100% {
+    opacity: 1;
+    -webkit-transform: scale(1);
+    transform: scale(1);
+  }
+}
+
+/*# sourceMappingURL=style.css.map */
+</style>
 
