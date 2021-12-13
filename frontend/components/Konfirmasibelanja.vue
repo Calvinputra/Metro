96d6b17873cc034f8756ac657257169f50954902
jasonrenata <54830225+jasonrenata@users.noreply.github.com>
2021-12-13 19:18:01 +0700

    pop up konfirmasi belanja

---
+++ frontend/components/Konfirmasibelanja.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <b-button v-b-modal.modal-center>Launch centered modal</b-button>
+
+    <b-modal id="modal-center" centered title="Konfirmasi">
+      <p class="my-4">
+        Terima kasih, barang yang anda pilih akan segera dikirim. Ingin lanjut
+        belanja?
+      </p>
+      <div class="d-flex">
+        <div class="mr-4">
+          <button type="button" class="btn btn-danger">Lanjut</button>
+        </div>
+        <div>
+          <button type="button" class="btn btn-light border-danger">
+            Selesai
+          </button>
+        </div>
+      </div>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
--- frontend/pages/old_test_modal.vue
+++ frontend/pages/old_test_modal.vue
@@ -7,7 +7,7 @@
     <b-button v-b-modal.modal-2>Detail Modal</b-button>
 
     <ModalUlasan />
-
+    <Konfirmasibelanja />
     <Footer />
   </section>
 </template>
