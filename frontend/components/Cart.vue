2fd3dc50069971f5ee5bad5c934bce937ee241f3
jasonrenata <54830225+jasonrenata@users.noreply.github.com>
2021-12-17 00:35:01 +0700

    Cart sementara

--- frontend/components/Cart.vue
+++ frontend/components/Cart.vue
@@ -1,58 +1,120 @@
 <template>
-  <tr>
-    <td>
-      <div class="product-item text-middle" style="display: flex">
-        <div class="form-check mt-auto mb-auto">
-          <input
-            class="form-check-input"
-            type="checkbox"
-            id="flexCheckChecked"
-            v-model="process_model"
-            @change="updateSubTotal"
-          />
-        </div>
-        <a class="product-thumb" href="#"
-          ><img
-            class="mt-4"
-            style="border-radius: 5%; width: 280px; height: 180px"
-            :src="ASSET_URL + '/' + JSON.parse(product.images)[0]"
-            alt="Product"
-        /></a>
-        <p class="row gx-0 align-items-center ml-5">
-          {{ product.code }} - {{ product.name }}
-        </p>
-      </div>
-      <pre></pre>
-    </td>
-    <td class="text-center align-middle">
-      <div class="count-input">
-        <input
-          @change="updateSubTotal()"
-          class="col-sm-4"
-          id="demoInput"
-          type="number"
-          min="0"
-          v-model="qty_model"
-        />
-      </div>
-    </td>
-    <td class="text-center text-lg text-medium align-middle">
-      Rp.{{ Number(product.price).toLocaleString("id-ID") }}
-    </td>
-    <td class="text-center text-lg text-medium align-middle">
-      Rp.{{ Number(sub_total).toLocaleString("id-ID") }}
-    </td>
-    <td class="text-center align-middle">
-      <a
-        class="remove-from-cart"
-        data-toggle="tooltip"
-        title=""
-        @click="deleteCart"
-        data-original-title="Remove item"
-        ><i class="fa fa-trash"></i
-      ></a>
-    </td>
-  </tr>
+  <section>
+    <div id="cart1-webview">
+      <tr>
+        <td>
+          <div class="product-item text-middle" style="display: flex">
+            <div class="form-check mt-auto mb-auto">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                id="flexCheckChecked"
+                v-model="process_model"
+                @change="updateSubTotal"
+              />
+            </div>
+            <a class="product-thumb" href="#"
+              ><img
+                class="mt-4"
+                style="border-radius: 5%; width: 280px; height: 180px"
+                :src="ASSET_URL + '/' + JSON.parse(product.images)[0]"
+                alt="Product"
+            /></a>
+            <p class="row gx-0 align-items-center ml-5">
+              {{ product.code }} - {{ product.name }}
+            </p>
+          </div>
+          <pre></pre>
+        </td>
+        <td class="text-center align-middle">
+          <div class="count-input">
+            <input
+              @change="updateSubTotal()"
+              class="col-sm-4"
+              id="demoInput"
+              type="number"
+              min="0"
+              v-model="qty_model"
+            />
+          </div>
+        </td>
+        <td class="text-center text-lg text-medium align-middle">
+          Rp.{{ Number(product.price).toLocaleString("id-ID") }}
+        </td>
+        <td class="text-center text-lg text-medium align-middle">
+          Rp.{{ Number(sub_total).toLocaleString("id-ID") }}
+        </td>
+        <td class="text-center align-middle">
+          <a
+            class="remove-from-cart"
+            data-toggle="tooltip"
+            title=""
+            @click="deleteCart"
+            data-original-title="Remove item"
+            ><i class="fa fa-trash"></i
+          ></a>
+        </td>
+      </tr>
+    </div>
+
+    <div id="cart1-mobileview">
+      <tr>
+        <td>
+          <div class="product-item text-middle" style="display: flex">
+            <div class="form-check mt-auto mb-auto">
+              <input
+                class="form-check-input"
+                type="checkbox"
+                id="flexCheckChecked"
+                v-model="process_model"
+                @change="updateSubTotal"
+              />
+            </div>
+            <a class="product-thumb" href="#"
+              ><img
+                class="mt-4"
+                style="border-radius: 5%; width: 280px; height: 180px"
+                :src="ASSET_URL + '/' + JSON.parse(product.images)[0]"
+                alt="Product"
+            /></a>
+            <p class="row gx-0 align-items-center ml-5">
+              {{ product.code }} - {{ product.name }}
+            </p>
+          </div>
+          <pre></pre>
+        </td>
+        <td class="text-center align-middle">
+          <div class="count-input">
+            <input
+              @change="updateSubTotal()"
+              class="col-sm-4"
+              id="demoInput"
+              type="number"
+              min="0"
+              v-model="qty_model"
+            />
+          </div>
+        </td>
+        <td class="text-center text-lg text-medium align-middle">
+          Rp.{{ Number(product.price).toLocaleString("id-ID") }}
+        </td>
+        <td class="text-center text-lg text-medium align-middle">
+          Rp.{{ Number(sub_total).toLocaleString("id-ID") }}
+        </td>
+        <td class="text-center align-middle">
+          <a
+            class="remove-from-cart"
+            data-toggle="tooltip"
+            title=""
+            @click="deleteCart"
+            data-original-title="Remove item"
+            ><i class="fa fa-trash"></i
+          ></a>
+        </td>
+      </tr>
+      <Footer />
+    </div>
+  </section>
 </template>
 <script>
 export default {
@@ -62,7 +124,7 @@ export default {
       sub_total: 0,
       ASSET_URL: process.env.ASSET_URL,
       qty_model: 0,
-      process_model: false,
+      process_model: false
     };
   },
   methods: {
@@ -74,7 +136,7 @@ export default {
         try {
           let data = {
             qty: this.qty_model,
-            process: this.process_model,
+            process: this.process_model
           };
 
           let response = await this.$axios
@@ -93,7 +155,7 @@ export default {
         this.$store.dispatch("updateCart", {
           product: this.product,
           qty: this.qty_model,
-          process: this.process_model,
+          process: this.process_model
         });
       }
     },
@@ -107,7 +169,7 @@ export default {
               this.$toast.success("Successfully delete a product from cart", {
                 theme: "bubble",
                 position: "bottom-right",
-                duration: 5000,
+                duration: 5000
               });
               this.$store.dispatch("setCartChange", true);
               this.$nuxt.refresh();
@@ -119,13 +181,36 @@ export default {
       } else {
         this.$store.dispatch("deleteCart", this.product);
       }
-    },
+    }
   },
 
   created() {
     this.sub_total = this.qty * this.product.price;
     this.qty_model = this.qty;
     this.process_model = this.process;
-  },
+  }
 };
 </script>
+
+<style lang="css" scoped>
+#cart1-mobileview {
+  display: none;
+}
+#cart1-mobileview {
+  display: none;
+}
+/* 0 - 991 px */
+@media screen and (max-width: 500px) {
+  #cart1_logo {
+    display: none !important;
+  }
+
+  #cart1-webview {
+    display: none;
+  }
+
+  #cart1-mobileview {
+    display: flex;
+  }
+}
+</style>
--- frontend/pages/Cart.vue
+++ frontend/pages/Cart.vue
@@ -1,96 +1,200 @@
 <template>
-  <section>
-    <Header />
-    <div class="container padding-bottom-3x mb-1">
-      <!-- Alert-->
-      <nav class="container my-4" aria-label="breadcrumb">
-        <ol class="py-1 breadcrumb">
-          <li class="my-2 ms-3 breadcrumb-item opacity-50">
-            <a style="text-decoration: none; color: black" href="/">Beranda</a>
-          </li>
-          <li
-            class="my-2 breadcrumb-item active opacity-50"
-            style="color: black"
-            aria-current="page"
-          >
-            Keranjang Saya
-          </li>
-        </ol>
-      </nav>
-      <h2>Keranjang Belanja</h2>
-      <pre></pre>
-      <!-- Shopping Cart-->
-      <div class="table-responsive shopping-cart">
-        <table class="table">
-          <thead>
-            <tr>
-              <th>Produk</th>
-              <th class="text-center">Jumlah</th>
-              <th class="text-center">Harga</th>
-              <th class="text-center">Sub total</th>
-              <th class="text-center">
-                <a
-                  @click="destroyAll"
-                  class="btn btn-sm btn-outline-danger"
-                  href="#"
-                  >Clear Cart</a
-                >
-              </th>
-            </tr>
-          </thead>
-          <tbody></tbody>
-          <template v-if="!this.$auth.loggedIn">
-            <Cart
-              v-for="cart in tempCart"
-              :key="cart.id"
-              :product="cart.product"
-              :qty="cart.qty"
-              :id="cart.id"
-              :process="cart.process"
-            />
-          </template>
+  <section class="container">
+    <div id="cart-webview">
+      <Header />
+      <div class="container padding-bottom-3x mb-1">
+        <!-- Alert-->
+        <nav class="container my-4" aria-label="breadcrumb">
+          <ol class="py-1 breadcrumb">
+            <li class="my-2 ms-3 breadcrumb-item opacity-50">
+              <a style="text-decoration: none; color: black" href="/"
+                >Beranda</a
+              >
+            </li>
+            <li
+              class="my-2 breadcrumb-item active opacity-50"
+              style="color: black"
+              aria-current="page"
+            >
+              Keranjang Saya
+            </li>
+          </ol>
+        </nav>
+        <h2>Keranjang Belanja</h2>
+        <pre></pre>
+        <!-- Shopping Cart-->
+        <div class="table-responsive shopping-cart">
+          <table class="table">
+            <thead>
+              <tr>
+                <th>Produk</th>
+                <th class="text-center">Jumlah</th>
+                <th class="text-center">Harga</th>
+                <th class="text-center">Sub total</th>
+                <th class="text-center">
+                  <a
+                    @click="destroyAll"
+                    class="btn btn-sm btn-outline-danger"
+                    href="#"
+                    >Clear Cart</a
+                  >
+                </th>
+              </tr>
+            </thead>
+            <tbody></tbody>
+            <template v-if="!this.$auth.loggedIn">
+              <Cart
+                v-for="cart in tempCart"
+                :key="cart.id"
+                :product="cart.product"
+                :qty="cart.qty"
+                :id="cart.id"
+                :process="cart.process"
+              />
+            </template>
 
-          <template v-if="this.$auth.loggedIn">
-            <Cart
-              v-for="cart in carts"
-              :key="cart.id"
-              :product="cart.product"
-              :qty="cart.qty"
-              :id="cart.id"
-              :process="cart.process"
-            />
-          </template>
-        </table>
-      </div>
-      <div class="text-right text-right col-sm-11">
-        <span class="text-center mr-5 pl-5">Total Harga</span>
-        <span class="pl-5"
-          >Rp. {{ Number(grandTotal).toLocaleString("id-ID") }}</span
-        >
-      </div>
-      <br />
-      <br />
-      <div class="shopping-cart-footer">
-        <div class="column"></div>
-      </div>
-      <div class="shopping-cart-footer">
-        <div class="column text-center">
-          <a
-            style="color: red"
-            class="btn btn-light"
-            href="#"
-            data-toast=""
-            data-toast-type="success"
-            data-toast-position="topRight"
-            data-toast-icon="icon-circle-check"
-            data-toast-title="Your cart"
-            data-toast-message="is updated successfully!"
-            >Lanjut Belanja</a
-          ><nuxt-link class="btn btn-danger" to="/checkout">Periksa</nuxt-link>
+            <template v-if="this.$auth.loggedIn">
+              <Cart
+                v-for="cart in carts"
+                :key="cart.id"
+                :product="cart.product"
+                :qty="cart.qty"
+                :id="cart.id"
+                :process="cart.process"
+              />
+            </template>
+          </table>
+        </div>
+        <div class="text-right text-right col-sm-11">
+          <span class="text-center mr-5 pl-5">Total Harga</span>
+          <span class="pl-5"
+            >Rp. {{ Number(grandTotal).toLocaleString("id-ID") }}</span
+          >
+        </div>
+        <br />
+        <br />
+        <div class="shopping-cart-footer">
+          <div class="column"></div>
+        </div>
+        <div class="shopping-cart-footer">
+          <div class="column text-center">
+            <a
+              style="color: red"
+              class="btn btn-light"
+              href="#"
+              data-toast=""
+              data-toast-type="success"
+              data-toast-position="topRight"
+              data-toast-icon="icon-circle-check"
+              data-toast-title="Your cart"
+              data-toast-message="is updated successfully!"
+              >Lanjut Belanja</a
+            ><nuxt-link class="btn btn-danger" to="/checkout"
+              >Periksa</nuxt-link
+            >
+          </div>
         </div>
       </div>
+      <Footer />
     </div>
-    <Footer />
+
+    <div id="cart-mobileview">
+      <Header />
+      <div class="container padding-bottom-3x mb-1">
+        <!-- Alert-->
+        <nav class="container my-4" aria-label="breadcrumb">
+          <ol class="py-1 breadcrumb">
+            <li class="my-2 ms-3 breadcrumb-item opacity-50">
+              <a style="text-decoration: none; color: black" href="/"
+                >Beranda</a
+              >
+            </li>
+            <li
+              class="my-2 breadcrumb-item active opacity-50"
+              style="color: black"
+              aria-current="page"
+            >
+              Keranjang Saya
+            </li>
+          </ol>
+        </nav>
+        <h2>Keranjang Belanja</h2>
+        <pre></pre>
+        <!-- Shopping Cart-->
+        <div class="table-responsive shopping-cart">
+          <table class="table">
+            <thead>
+              <tr>
+                <th>Produk</th>
+                <th class="text-center">Jumlah</th>
+                <th class="text-center">Harga</th>
+                <th class="text-center">Sub total</th>
+                <th class="text-center">
+                  <a
+                    @click="destroyAll"
+                    class="btn btn-sm btn-outline-danger"
+                    href="#"
+                    >Clear Cart</a
+                  >
+                </th>
+              </tr>
+            </thead>
+            <tbody></tbody>
+            <template v-if="!this.$auth.loggedIn">
+              <Cart
+                v-for="cart in tempCart"
+                :key="cart.id"
+                :product="cart.product"
+                :qty="cart.qty"
+                :id="cart.id"
+                :process="cart.process"
+              />
+            </template>
+
+            <template v-if="this.$auth.loggedIn">
+              <Cart
+                v-for="cart in carts"
+                :key="cart.id"
+                :product="cart.product"
+                :qty="cart.qty"
+                :id="cart.id"
+                :process="cart.process"
+              />
+            </template>
+          </table>
+        </div>
+        <div class="text-right text-right col-sm-11">
+          <span class="text-center mr-5 pl-5">Total Harga</span>
+          <span class="pl-5"
+            >Rp. {{ Number(grandTotal).toLocaleString("id-ID") }}</span
+          >
+        </div>
+        <br />
+        <br />
+        <div class="shopping-cart-footer">
+          <div class="column"></div>
+        </div>
+        <div class="shopping-cart-footer">
+          <div class="column text-center">
+            <a
+              style="color: red"
+              class="btn btn-light"
+              href="#"
+              data-toast=""
+              data-toast-type="success"
+              data-toast-position="topRight"
+              data-toast-icon="icon-circle-check"
+              data-toast-title="Your cart"
+              data-toast-message="is updated successfully!"
+              >Lanjut Belanja</a
+            ><nuxt-link class="btn btn-danger" to="/checkout"
+              >Periksa</nuxt-link
+            >
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
   </section>
 </template>
 
@@ -193,3 +297,22 @@ function decrement() {
   document.getElementById("demoInput").stepDown();
 }
 </script>
+
+<style lang="css" scoped>
+#cart-mobileview {
+  display: none;
+}
+/* 0 - 991 px */
+@media screen and (max-width: 500px) {
+  #cart_logo {
+    display: none !important;
+  }
+
+  #cart-webview {
+    display: none;
+  }
+  #cart-mobileview {
+    display: grid;
+  }
+}
+</style>
