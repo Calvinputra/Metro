584df590a9f7a6d2e76c62fa685567f8593043e5
gabriella <gabriellafranchesca22@gmail.com>
2021-12-17 15:36:56 +0700

    carousel detail product udah

--- frontend/pages/products/_id.vue
+++ frontend/pages/products/_id.vue
@@ -225,116 +225,27 @@
         <Breadcrumb :links="breadcrumb" />
         <div class="container">
           <div class="row">
-            <div
-              id="carouselExampleIndicators"
-              class="carousel slide"
-              data-bs-ride="carousel"
-            >
-              <div class="carousel-indicators">
-                <button
-                  type="button"
-                  data-bs-target="#carouselExampleIndicators"
-                  data-bs-slide-to="0"
-                  class="active"
-                  aria-current="true"
-                  aria-label="Slide 1"
-                ></button>
-                <button
-                  type="button"
-                  data-bs-target="#carouselExampleIndicators"
-                  data-bs-slide-to="1"
-                  aria-label="Slide 2"
-                ></button>
-                <button
-                  type="button"
-                  data-bs-target="#carouselExampleIndicators"
-                  data-bs-slide-to="2"
-                  aria-label="Slide 3"
-                ></button>
-              </div>
-              <div class="carousel-inner">
-                <div class="carousel-item active">
-                  <img
-                    src="/img/emojione_star.png"
-                    class="d-block w-100"
-                    alt="..."
-                  />
-                </div>
-                <div class="carousel-item">
-                  <img
-                    src="/img/tokopedia.png"
-                    class="d-block w-100"
-                    alt="..."
-                  />
-                </div>
-                <div class="carousel-item">
-                  <img
-                    src="/img/emojione_star.png"
-                    class="d-block w-100"
-                    alt="..."
-                  />
-                </div>
-              </div>
-              <button
-                class="carousel-control-prev"
-                type="button"
-                data-bs-target="#carouselExampleIndicators"
-                data-bs-slide="prev"
+            <div>
+              <b-carousel
+                id="carousel-1"
+                v-model="slide"
+                :interval="4000"
+                controls
+                indicators
+                img-width="100%"
+                img-height="480"
+                style="text-shadow: 1px 1px 2px #333"
+                @sliding-start="onSlideStart"
+                @sliding-end="onSlideEnd"
               >
-                <span
-                  class="carousel-control-prev-icon"
-                  aria-hidden="true"
-                ></span>
-                <span class="visually-hidden">Previous</span>
-              </button>
-              <button
-                class="carousel-control-next"
-                type="button"
-                data-bs-target="#carouselExampleIndicators"
-                data-bs-slide="next"
-              >
-                <span
-                  class="carousel-control-next-icon"
-                  aria-hidden="true"
-                ></span>
-                <span class="visually-hidden">Next</span>
-              </button>
+                <b-carousel-slide
+                  v-for="(img, itemObjKey) in JSON.parse(data.images)"
+                  :key="itemObjKey"
+                  :img-src="ASSET_URL + '/' + img"
+                >
+                </b-carousel-slide>
+              </b-carousel>
             </div>
-            <!-- <div class="preview col-md-6">
-              <div class="preview-pic tab-content">
-                <template v-for="(img, itemObjKey) in JSON.parse(data.images)">
-                  <div
-                    class="tab-pane"
-                    :id="'pic-' + itemObjKey"
-                    :key="'pic-' + itemObjKey"
-                  >
-                    <img
-                      :src="ASSET_URL + '/' + img"
-                      alt="Gallery image 1"
-                      class="ecommerce-gallery-main-img active w-100"
-                    />
-                  </div>
-                </template>
-
-                <div class="tab-pane" id="pic-2">
-                  <img :src="ASSET_URL + '/' + JSON.parse(data.images)[1]" />
-                </div>
-              </div>
-              <ul class="preview-thumbnail nav nav-tabs">
-                <template v-for="(img, itemObjKey) in JSON.parse(data.images)">
-                  <li class="" :key="'pic' + itemObjKey">
-                    <a :data-target="'#pic-' + itemObjKey" data-toggle="tab"
-                      ><img :src="ASSET_URL + '/' + img"
-                    /></a>
-                  </li>
-                </template>
-                <li>
-                  <a data-target="#pic-2" data-toggle="tab"
-                    ><img :src="ASSET_URL + '/' + JSON.parse(data.images)[1]"
-                  /></a>
-                </li>
-              </ul>
-            </div> -->
             <div class="col-sm-6 mt-5 container">
               <h5>Nama Merek</h5>
               <div class="row">
@@ -567,9 +478,12 @@
       type="text/javascript"
       src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"
     ></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+    ></script>
   </section>
-  
 </template>
 
 <style lang="css" scoped>
@@ -600,6 +514,8 @@ export default {
   data() {
     return {
       ASSET_URL: process.env.ASSET_URL,
+      slide: 0,
+      sliding: null,
     };
   },
   async asyncData({ $axios, params }) {
@@ -635,6 +551,12 @@ export default {
     }
   },
   methods: {
+    onSlideStart(slide) {
+      this.sliding = true;
+    },
+    onSlideEnd(slide) {
+      this.sliding = false;
+    },
     async addToWishList(id) {
       try {
         if (this.$auth.loggedIn) {
