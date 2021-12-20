de42785fb4b220c936a4716fc3a13a70ef37a853
Jonathan <jonathanchang96@gmail.com>
2021-12-20 20:48:45 +0700

    swiper example

--- frontend/pages/Home.vue
+++ frontend/pages/Home.vue
@@ -64,6 +64,7 @@
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     </section>
+    <SwiperCardProduct :products="products" />
   </section>
 </template>
 
