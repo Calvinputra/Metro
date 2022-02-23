800e0ffd4e5f44834158a88be50630ae098e932a
jasonrenata <54830225+jasonrenata@users.noreply.github.com>
2022-02-23 14:41:04 +0700

    hide footer modal

--- frontend/components/ModalUlasan.vue
+++ frontend/components/ModalUlasan.vue
@@ -1,70 +1,70 @@
 <template>
   <div style="background-color: white !important; font-family: 'Nunito Sans'">
-    <div class="modal" ref="modal-ulasan" id="modal-ulasan" tabindex="-1">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">Beri Rating Ulasan</h5>
-          </div>
-          <div class="modal-body">
-            <div class="col-sm-12 mb-3 mt-3 text-center">
-              <img
-                class="img-fluid max-width:100% height:auto rounded"
-                src="/img/spcBody.jpg"
-                alt=""
-              />
-              <div>
-                <div class="mt-2 mb-2">
-                  <p>
-                    <!-- {{
+    <b-modal
+      hide-footer
+      ref="modal-ulasan"
+      id="modal-ulasan"
+      title="Beri Ulasan"
+    >
+      <h5 class="text-center fw-bold">Beri Rating Ulasan</h5>
+      <div class="col-sm-12 mb-3 mt-3 text-center">
+        <img
+          class="img-fluid max-width:100% height:auto rounded"
+          src="/img/spcBody.jpg"
+          alt=""
+        />
+        <div>
+          <div class="mt-2 mb-2">
+            <p>
+              <!-- {{
                 typeof detail.selectedDetail !== "undefined"
                   ? detail.selectedDetail.product.code
                   : ""
               }} -->
-                    -
-                  </p>
-                  <!-- <p>{{ detail.selectedDetail.name }}</p> -->
-                </div>
-                <div class="">
-                  <template v-for="index in 5">
-                    <i
-                      class="me-2"
-                      :key="`rating-icon-${index}`"
-                      :class="`${rating >= index ? 'fas' : 'far'} fa-star`"
-                      @click="changeRatingHandler(index)"
-                      style="font-size: 40px !important"
-                      aria-hidden="true"
-                    ></i>
-                  </template>
-                </div>
-              </div>
-            </div>
-            <div class="">
-              <p class="text-center">
-                Hai {{ $auth.user.first_name }}, puas dengan barang yang kamu
-                beli? Beri ulasan dan rekomendasikan ke pembeli lain ya!
-              </p>
-            </div>
-            <div class="mt-4">
-              <label for="review"
-                >Beri Komentar<span style="color: red">*</span>:</label
-              >
-              <textarea
-                class="form-control col-sm-12"
-                id="review"
-                name="review"
-                v-model="review"
-                rows="3"
-                placeholder="Tulis ulasan di sini..."
-              ></textarea>
-            </div>
-            <br />
+              -
+            </p>
+            <!-- <p>{{ detail.selectedDetail.name }}</p> -->
+          </div>
+          <div class="">
+            <template v-for="index in 5">
+              <i
+                class="me-2"
+                :key="`rating-icon-${index}`"
+                :class="`${rating >= index ? 'fas' : 'far'} fa-star`"
+                @click="changeRatingHandler(index)"
+                style="font-size: 40px !important"
+                aria-hidden="true"
+              ></i>
+            </template>
+          </div>
+        </div>
+      </div>
+      <div class="">
+        <p class="text-center">
+          Hai {{ $auth.user.first_name }}, puas dengan barang yang kamu beli?
+          Beri ulasan dan rekomendasikan ke pembeli lain ya!
+        </p>
+      </div>
+      <div class="mt-4">
+        <label for="review"
+          >Beri Komentar<span style="color: red">*</span>:</label
+        >
+        <textarea
+          class="form-control col-sm-12"
+          id="review"
+          name="review"
+          v-model="review"
+          rows="3"
+          placeholder="Tulis ulasan di sini..."
+        ></textarea>
+      </div>
+      <br />
 
-            <div class="text-center">
-              <button
-                @click="onSubmitHandler"
-                type="submit"
-                class="
+      <div class="text-center mb-4">
+        <button
+          @click="onSubmitHandler"
+          type="submit"
+          class="
             btn
             text-danger
             btn-light btn-sm
@@ -74,24 +74,11 @@
             py-2
             px-2
           "
-              >
-                Kirim
-              </button>
-            </div>
-          </div>
-          <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-secondary"
-              data-bs-dismiss="modal"
-            >
-              Close
-            </button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-          </div>
-        </div>
+        >
+          Kirim
+        </button>
       </div>
-    </div>
+    </b-modal>
   </div>
 </template>
 
