d411db6280d8ba18c856acca5c4032d387201c47
jasonrenata <54830225+jasonrenata@users.noreply.github.com>
2022-03-02 15:52:59 +0700

    new file for cart tab and search tab

---
+++ frontend/components/Carttab.vue
@@ -0,0 +1,160 @@
+<template>
+  <tr style="font-size:14px;">
+    <td>
+      <div class="product-item text-middle" style="display: flex;">
+        <div class="form-check mt-auto mb-auto">
+          <input
+            class="form-check-input p-2 me-3"
+            type="checkbox"
+            id="flexCheckChecked"
+            v-model="process_model"
+            @change="updateSubTotal"
+          />
+        </div>
+        <a class="product-thumb" href="#"
+          ><img
+            class="mt-4"
+            style="border-radius: 5%; width: 150px; height: 120px"
+            :src="ASSET_URL + '/' + JSON.parse(product.images)[0]"
+            alt="Product"
+        /></a>
+        <p class="row gx-0 align-items-center ml-5 pt-3">
+          {{ product.code }} - {{ product.name }}
+        </p>
+      </div>
+      <pre></pre>
+    </td>
+    <td class="text-center align-middle pt-0">
+      <div class="count-input">
+        <input
+          @change="updateSubTotal()"
+          class="col-sm-11"
+          id="demoInput"
+          type="number"
+          min="0"
+          v-model="qty_model"
+        />
+      </div>
+    </td>
+    <td class="text-center text-lg text-medium align-middle pt-0">
+      Rp.{{ Number(product.price).toLocaleString("id-ID") }}
+    </td>
+    <td class="text-center text-lg text-medium align-middle pt-0">
+      Rp.{{ Number(sub_total).toLocaleString("id-ID") }}
+    </td>
+    <td class="text-center align-middle pt-0">
+      <a
+        class="remove-from-cart"
+        data-toggle="tooltip"
+        title=""
+        @click="confirmationDeleteCart"
+        data-original-title="Remove item"
+        ><i class="fa fa-trash"></i
+      ></a>
+    </td>
+  </tr>
+</template>
+<script>
+export default {
+  props: ["product", "qty", "id", "process", "index"],
+  data() {
+    return {
+      sub_total: 0,
+      ASSET_URL: process.env.ASSET_URL,
+      qty_model: 0,
+      process_model: false
+    };
+  },
+  methods: {
+    async updateSubTotal() {
+      if (this.$auth.loggedIn) {
+        console.log("checked : " + this.process_model);
+        this.sub_total = this.qty_model * this.product.price;
+        //update database
+        try {
+          let data = {
+            qty: this.qty_model,
+            process: this.process_model
+          };
+
+          let response = await this.$axios
+            .$put(process.env.API_URL + "/api/carts/" + this.id, data)
+            .then(() => {
+              //this.$nuxt.refresh();
+
+              this.$emit("updateCart", {
+                qty: this.qty_model,
+                index: this.index,
+                process: this.process_model
+              });
+            });
+
+          console.log(response);
+        } catch (error) {
+          console.log(error);
+        }
+      } else {
+        this.sub_total = this.qty_model * this.product.price;
+        this.$store.dispatch("updateCart", {
+          product: this.product,
+          qty: this.qty_model,
+          process: this.process_model
+        });
+      }
+    },
+
+    async deleteCart() {
+      if (this.$auth.loggedIn) {
+        try {
+          let response = await this.$axios
+            .$delete(process.env.API_URL + "/api/carts/" + this.id)
+            .then(() => {
+              this.$toast.success("Berhasil menghapus produk ini", {
+                theme: "bubble",
+                position: "bottom-right",
+                duration: 5000
+              });
+              this.$emit("deleteCartHandler", this.index);
+            });
+          console.log(response);
+        } catch (error) {
+          console.log(error);
+        }
+      } else {
+        this.$store.dispatch("deleteCart", this.product);
+      }
+    },
+    async confirmationDeleteCart() {
+      this.$bvModal
+        .msgBoxConfirm("Konfirmasi untuk membatalkan pembelian produk ini", {
+          title: "Konfirmasi Pembatalan",
+          size: "sm",
+          buttonSize: "sm",
+          okVariant: "danger",
+          okTitle: "Ya",
+          cancelTitle: "Tidak",
+          footerClass: "p-2",
+          hideHeaderClose: false,
+          centered: true
+        })
+        .then(value => {
+          this.boxTwo = value;
+          if (value) {
+            //console.log("Yes Clicked"+value);
+
+            this.deleteCart();
+          }
+        })
+        .catch(err => {
+          // An error occurred
+        });
+    }
+  },
+
+  created() {
+    this.sub_total = this.qty * this.product.price;
+    this.qty_model = this.qty;
+    this.process_model = this.process;
+  }
+};
+</script>
---
+++ frontend/components/Searchtab.vue
@@ -0,0 +1,51 @@
+<template>
+  <!-- search -->
+  <div id="search" class="nav-item align-self-start mt-2">
+    <div class="d-flex">
+      <input
+        class="form-control-lg"
+        style="border-radius: 10px 0px 0px 10px"
+        type="search"
+        placeholder="Cari Barang Di sini"
+        aria-label="Search"
+        name="s"
+        v-model="query"
+        v-on:keyup.enter="onSearch"
+      />
+      <button
+        class="btn btn-outline-success mx-auto pe-5 ps-3"
+        style="
+          background-color: #e5e5e5;
+          width: 40px;
+          
+          border-radius: 0px 10px 10px 0px;
+        "
+        type="submit"
+        @click="onSearch"
+      >
+        <i
+          class="fas fa-search"
+          style="font-size: 30px !important; color: black !important"
+        ></i>
+      </button>
+    </div>
+    <!-- kategory -->
+    <div class=""></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["data"],
+  data() {
+    return {
+      query: ""
+    };
+  },
+  methods: {
+    onSearch() {
+      this.$router.push({ path: "/products", query: { s: this.query } });
+    }
+  }
+};
+</script>
