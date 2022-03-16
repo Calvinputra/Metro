1196293922eecd5ecc5448218d8666d6fba3e57e
Jason Renata <54830225+jasonrenata@users.noreply.github.com>
2022-03-16 11:33:50 +0700

    kebijakan privasi

--- frontend/pages/kebijakanprivasi.vue
+++ frontend/pages/kebijakanprivasi.vue
@@ -8,20 +8,11 @@
               <AccountMenu :name="'register'" />
             </div>
             <div
-              class="
-                col-sm-9
-                offset-md-1
-                align-self-start
-                mt-2
-                row
-                justify-content-between
-              "
+              class="col-sm-9 offset-md-1 align-self-start mt-2 row justify-content-between"
             >
               <div class="col-sm-12">
                 <label class="mb-0"
-                  ><h2 class="font-weight-bold">
-                    Kebijakan Privasi
-                  </h2></label
+                  ><h2 class="font-weight-bold">Kebijakan Privasi</h2></label
                 >
               </div>
               <div class="mb-4">
@@ -33,53 +24,275 @@
               </div>
               <div>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
-                  sit amet dui non metus aliquet laoreet in mollis lorem.
-                  Interdum et malesuada fames ac ante ipsum primis in faucibus.
-                  Proin sollicitudin, dolor ut aliquet euismod, erat libero
-                  congue dolor, ac tempus dolor erat vel quam. Nulla tincidunt
-                  augue tincidunt, aliquet nisi eget, mattis velit. Mauris
-                  tincidunt, justo ac eleifend faucibus, risus lacus porta odio,
-                  eu iaculis enim orci a arcu. Ut mollis, lorem a finibus
-                  convallis, nunc nisl congue orci, eu viverra elit libero vel
-                  massa. Cras sit amet nibh et mauris luctus condimentum. Sed
-                  quis eleifend lacus.
+                  Metro Jaya Indonesia (“Kami”) berkomitmen untuk melindungi dan
+                  menghormati privasi pengguna situs
+                  <a
+                    class="text-decoration-none"
+                    style="color: red"
+                    href="https://metrojayaindonesia.com/"
+                  >
+                    https://metrojayaindonesia.com/
+                  </a>
+                  Kebijakan Privasi ini mengatur landasan dasar mengenai
+                  bagaimana Kami menggunakan informasi pribadi (“Data Pribadi”)
+                  yang Kami kumpulkan dan/atau Anda berikan. Mohon membaca
+                  Kebijakan Privasi Kami dengan seksama sehingga Anda dapat
+                  memahami cara Kami dalam menggunakan Data Pribadi tersebut.
+                  Dengan mengunjungi situs Kami, Anda menerima dan menyetujui
+                  pendekatan dan cara-cara yang digambarkan dalam Kebijakan
+                  Privasi ini. Kami biasanya mengumpulkan dua jenis informasi
+                  berbeda di situs web kami: Informasi Otomatis dan Informasi
+                  yang Anda Berikan. Apa itu Informasi Otomatis? Saat Anda
+                  berinteraksi dengan situs web, ia menerima jenis informasi
+                  anonim tertentu. Kami menggunakan "cookie" untuk mengumpulkan
+                  data ini, yang kemudian kami simpan dan gunakan untuk
+                  melakukan perbaikan pada situs kami dan menciptakan pengalaman
+                  pengguna yang lebih relevan. Kami juga menggunakan informasi
+                  otomatis yang dikumpulkan untuk analisis, pengoptimalan situs,
+                  dan untuk menyesuaikan iklan dan pemasaran kami. Beberapa data
+                  anonim yang kami ambil adalah versi sistem operasi Anda, versi
+                  browser, dan alamat IP. Apa itu Informasi yang Anda Berikan?
+                  Informasi yang Anda kirim melalui formulir online kami,
+                  seperti nama Anda, nomor telepon, dan alamat email, diambil
+                  dan disimpan agar Metro Jaya Indonesia dapat melakukan kontak
+                  melalui nomor telepon maupun alamat email yang telah Anda dan
+                  pengunjung kami yang lain berikan baik yang berhubungan dengan
+                  proses transaksi di website ataupun tidak. Dengan mengirimkan
+                  informasi secara sukarela, Anda memberi wewenang Metro jaya
+                  Indonesia untuk menghubungi Anda melalui telepon, teks, email
+                  atau surat. Bagaimana informasi saya dapat digunakan oleh
+                  METRO JAYA Indonesia?
                 </p>
                 <p class="mt-3">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
-                  sit amet dui non metus aliquet laoreet in mollis lorem.
-                  Interdum et malesuada fames ac ante ipsum primis in faucibus.
-                  Proin sollicitudin, dolor ut aliquet euismod, erat libero
-                  congue dolor, ac tempus dolor erat vel quam. Nulla tincidunt
-                  augue tincidunt, aliquet nisi eget, mattis velit. Mauris
-                  tincidunt, justo ac eleifend faucibus, risus lacus porta odio,
-                  eu iaculis enim orci a arcu. Ut mollis, lorem a finibus
-                  convallis, nunc nisl congue orci, eu viverra elit libero vel
-                  massa. Cras sit amet nibh et mauris luctus condimentum. Sed
-                  quis eleifend lacus:
+                  1.
+                  <br />
+                  Informasi Otomatis. Kami akan menggunakan informasi ini: ·
+                  untuk mengelola situs dan operasi internal Kami, termasuk
+                  pencarian sumber masalah (troubleshooting), analisis data,
+                  pengujian, penelitian, serta tujuan-tujuan statistik dan
+                  survei lainnya; · untuk memperbaiki situs Kami sehingga konten
+                  dipastikan dapat disajikan dengan cara yang paling efektif
+                  untuk Anda; · untuk memungkinkan Anda berpartisipasi dalam
+                  fitur interaktif layanan Kami; sebagai bagian dari usaha dalam
+                  memastikan keselamatan dan keamanan situs Kami; · untuk
+                  mengukur dan memahami efektivitas periklanan yang Kami lakukan
+                  kepada Anda dan pihak lain, serta menyajikan iklan produk dan
+                  jasa yang relevan bagi Anda; · untuk memberi masukan dan
+                  rekomendasi kepada Anda dan pengguna lain dalam situs Kami
+                  mengenai produk dan jasa yang dapat menarik minat Anda dan
+                  mereka.
+                  <br />
+                  <br />
+                  2.
+                  <br />
+                  Informasi yang Anda berikan. Kami akan menggunakan informasi
+                  ini: · untuk menjalankan kewajiban Kami dalam menyediakan
+                  informasi, produk, dan jasa kepada Anda; · untuk menyediakan
+                  informasi terkait produk dan jasa lain yang Kami tawarkan;
+                  guna menyediakan Anda, atau mengizinkan pihak ketiga untuk
+                  menyediakan Anda informasi tentang produk dan jasa yang Kami
+                  anggap dapat menarik minat Anda. Jika Anda adalah pelanggan
+                  lama, Kami dapat menghubungi Anda secara elektronik atau
+                  cara-cara lain dengan informasi tentang produk dan jasa Kami.
+                  Jika Anda adalah pelanggan baru, dan di mana Kami mengizinkan
+                  pihak ketiga untuk menggunakan Data Pribadi Anda, Kami (atau
+                  mereka) dapat menghubungi Anda secara elektronik hanya jika
+                  Anda sudah memberikan persetujuan untuk memberitahukan Anda
+                  tentang perubahan pada jasa Kami; · untuk memastikan bahwa
+                  konten dari situs Kami disajikan dengan cara yang paling
+                  efektif bagi Anda.
                 </p>
               </div>
-              <div>
-                <ul class="text-black mt-3 mb-3">
-                  <li>1. asldkas</li>
-                  <li>2. asdmaksmd</li>
-                  <li>3. asdmaksmd</li>
-                  <li>4. asdmaksmd</li>
-                </ul>
-              </div>
-              <div>
+
+              <div class="mt-3">
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
-                  sit amet dui non metus aliquet laoreet in mollis lorem.
-                  Interdum et malesuada fames ac ante ipsum primis in faucibus.
-                  Proin sollicitudin, dolor ut aliquet euismod, erat libero
-                  congue dolor, ac tempus dolor erat vel quam. Nulla tincidunt
-                  augue tincidunt,
+                  Apa yang harus saya ketahui tentang cookie? Dengan menggunakan
+                  situs
+                  <a
+                    class="text-decoration-none"
+                    style="color: red"
+                    href="https://metrojayaindonesia.com/"
+                  >
+                    https://metrojayaindonesia.com/
+                  </a>
+                  , Anda mengakui dan menyetujui penggunaan cookie kami. Cookies
+                  adalah istilah yang menjelaskan potongan kecil data yang
+                  dikirim dari situs web ke browser Anda. Mereka biasanya
+                  digunakan untuk alasan teknis seperti analisis web. Kami
+                  menggunakan (cookies) untuk membedakan Anda dari pengguna lain
+                  pada situs Kami. Ini membantu Kami untuk menyediakan
+                  pengalaman yang baik ketika Anda merambah situs Kami, serta
+                  membantu Kami memperbaiki Platform Kami. Anda dapat menghapus
+                  cookie melalui browser Anda kapan saja. Anda bisa menghapusnya
+                  secara berkala. Bisakah syarat dan kebijakan ini berubah? Ya,
+                  kami berhak atas kebijakan kami sendiri untuk mengubah atau
+                  memodifikasi bagian dari Ketentuan Penggunaan dan Kebijakan
+                  Privasi kami setiap saat. Kami menyarankan Anda membaca
+                  kembali halaman ini secara berkala untuk mendapatkan informasi
+                  terbaru. Terakhir diubah: 6 Maret 2022
                 </p>
               </div>
             </div>
           </div>
         </div>
+      </section>
+
+      <client-only>
+        <!-- include VueJS first -->
+        <script src="https://unpkg.com/vue@latest"></script>
+
+        <!-- use the latest vue-select release -->
+        <script src="https://unpkg.com/vue-select@latest"></script>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/vue-select@latest/dist/vue-select.css"
+        />
+
+        <!-- or point to a specific vue-select release -->
+        <script src="https://unpkg.com/vue-select@3.0.0"></script>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/vue-select@3.0.0/dist/vue-select.css"
+        />
+      </client-only>
+    </section>
+    <section id="profile-mobileview" class="mt-3">
+      <section>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-2">
+              <AccountMenu :name="'register'" />
+            </div>
+            <div
+              class="col-sm-9 offset-md-1 align-self-start mt-2 row justify-content-between"
+            >
+              <div class="col-sm-12">
+                <label class="mb-0"
+                  ><h2 class="font-weight-bold">Kebijakan Privasi</h2></label
+                >
+              </div>
+              <div class="mb-4">
+                <hr
+                  style="height: 10%; width: 100%; border-width: 0; color: red"
+                  class="col-sm-12 mb-0 mt-0"
+                />
+                <p class="mb-">lorem ipsum</p>
+              </div>
+              <div>
+                <p>
+                  Metro Jaya Indonesia (“Kami”) berkomitmen untuk melindungi dan
+                  menghormati privasi pengguna situs
+                  <a
+                    class="text-decoration-none"
+                    style="color: red"
+                    href="https://metrojayaindonesia.com/"
+                  >
+                    https://metrojayaindonesia.com/
+                  </a>
+                  Kebijakan Privasi ini mengatur landasan dasar mengenai
+                  bagaimana Kami menggunakan informasi pribadi (“Data Pribadi”)
+                  yang Kami kumpulkan dan/atau Anda berikan. Mohon membaca
+                  Kebijakan Privasi Kami dengan seksama sehingga Anda dapat
+                  memahami cara Kami dalam menggunakan Data Pribadi tersebut.
+                  Dengan mengunjungi situs Kami, Anda menerima dan menyetujui
+                  pendekatan dan cara-cara yang digambarkan dalam Kebijakan
+                  Privasi ini. Kami biasanya mengumpulkan dua jenis informasi
+                  berbeda di situs web kami: Informasi Otomatis dan Informasi
+                  yang Anda Berikan. Apa itu Informasi Otomatis? Saat Anda
+                  berinteraksi dengan situs web, ia menerima jenis informasi
+                  anonim tertentu. Kami menggunakan "cookie" untuk mengumpulkan
+                  data ini, yang kemudian kami simpan dan gunakan untuk
+                  melakukan perbaikan pada situs kami dan menciptakan pengalaman
+                  pengguna yang lebih relevan. Kami juga menggunakan informasi
+                  otomatis yang dikumpulkan untuk analisis, pengoptimalan situs,
+                  dan untuk menyesuaikan iklan dan pemasaran kami. Beberapa data
+                  anonim yang kami ambil adalah versi sistem operasi Anda, versi
+                  browser, dan alamat IP. Apa itu Informasi yang Anda Berikan?
+                  Informasi yang Anda kirim melalui formulir online kami,
+                  seperti nama Anda, nomor telepon, dan alamat email, diambil
+                  dan disimpan agar Metro Jaya Indonesia dapat melakukan kontak
+                  melalui nomor telepon maupun alamat email yang telah Anda dan
+                  pengunjung kami yang lain berikan baik yang berhubungan dengan
+                  proses transaksi di website ataupun tidak. Dengan mengirimkan
+                  informasi secara sukarela, Anda memberi wewenang Metro jaya
+                  Indonesia untuk menghubungi Anda melalui telepon, teks, email
+                  atau surat. Bagaimana informasi saya dapat digunakan oleh
+                  METRO JAYA Indonesia?
+                </p>
+                <p class="mt-3">
+                  1.
+                  <br />
+                  Informasi Otomatis. Kami akan menggunakan informasi ini: ·
+                  untuk mengelola situs dan operasi internal Kami, termasuk
+                  pencarian sumber masalah (troubleshooting), analisis data,
+                  pengujian, penelitian, serta tujuan-tujuan statistik dan
+                  survei lainnya; · untuk memperbaiki situs Kami sehingga konten
+                  dipastikan dapat disajikan dengan cara yang paling efektif
+                  untuk Anda; · untuk memungkinkan Anda berpartisipasi dalam
+                  fitur interaktif layanan Kami; sebagai bagian dari usaha dalam
+                  memastikan keselamatan dan keamanan situs Kami; · untuk
+                  mengukur dan memahami efektivitas periklanan yang Kami lakukan
+                  kepada Anda dan pihak lain, serta menyajikan iklan produk dan
+                  jasa yang relevan bagi Anda; · untuk memberi masukan dan
+                  rekomendasi kepada Anda dan pengguna lain dalam situs Kami
+                  mengenai produk dan jasa yang dapat menarik minat Anda dan
+                  mereka.
+                  <br />
+                  <br />
+                  2.
+                  <br />
+                  Informasi yang Anda berikan. Kami akan menggunakan informasi
+                  ini: · untuk menjalankan kewajiban Kami dalam menyediakan
+                  informasi, produk, dan jasa kepada Anda; · untuk menyediakan
+                  informasi terkait produk dan jasa lain yang Kami tawarkan;
+                  guna menyediakan Anda, atau mengizinkan pihak ketiga untuk
+                  menyediakan Anda informasi tentang produk dan jasa yang Kami
+                  anggap dapat menarik minat Anda. Jika Anda adalah pelanggan
+                  lama, Kami dapat menghubungi Anda secara elektronik atau
+                  cara-cara lain dengan informasi tentang produk dan jasa Kami.
+                  Jika Anda adalah pelanggan baru, dan di mana Kami mengizinkan
+                  pihak ketiga untuk menggunakan Data Pribadi Anda, Kami (atau
+                  mereka) dapat menghubungi Anda secara elektronik hanya jika
+                  Anda sudah memberikan persetujuan untuk memberitahukan Anda
+                  tentang perubahan pada jasa Kami; · untuk memastikan bahwa
+                  konten dari situs Kami disajikan dengan cara yang paling
+                  efektif bagi Anda.
+                </p>
+              </div>
+
+              <div class="mt-3">
+                <p>
+                  Apa yang harus saya ketahui tentang cookie? Dengan menggunakan
+                  situs
+                  <a
+                    class="text-decoration-none"
+                    style="color: red"
+                    href="https://metrojayaindonesia.com/"
+                  >
+                    https://metrojayaindonesia.com/
+                  </a>
+                  , Anda mengakui dan menyetujui penggunaan cookie kami. Cookies
+                  adalah istilah yang menjelaskan potongan kecil data yang
+                  dikirim dari situs web ke browser Anda. Mereka biasanya
+                  digunakan untuk alasan teknis seperti analisis web. Kami
+                  menggunakan (cookies) untuk membedakan Anda dari pengguna lain
+                  pada situs Kami. Ini membantu Kami untuk menyediakan
+                  pengalaman yang baik ketika Anda merambah situs Kami, serta
+                  membantu Kami memperbaiki Platform Kami. Anda dapat menghapus
+                  cookie melalui browser Anda kapan saja. Anda bisa menghapusnya
+                  secara berkala. Bisakah syarat dan kebijakan ini berubah? Ya,
+                  kami berhak atas kebijakan kami sendiri untuk mengubah atau
+                  memodifikasi bagian dari Ketentuan Penggunaan dan Kebijakan
+                  Privasi kami setiap saat. Kami menyarankan Anda membaca
+                  kembali halaman ini secara berkala untuk mendapatkan informasi
+                  terbaru. Terakhir diubah: 6 Maret 2022
+                </p>
+              </div>
+            </div>
+          </div>
+          <Footermobile />
+        </div>
+        <Footer2mobile />
       </section>
 
       <client-only>
