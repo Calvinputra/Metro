44c6f640a9ef2c6429111bf87a2476467959df0e
Jonathan <jonathanchang96@gmail.com>
2022-03-18 21:46:50 +0700

    config

--- backend-laravel/config/app.php
+++ backend-laravel/config/app.php
@@ -177,7 +177,7 @@ return [
         OwenIt\Auditing\AuditingServiceProvider::class,
         FruitCake\Cors\CorsServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
-
+        Laravel\Socialite\SocialiteServiceProvider::class
     ],
 
     /*
@@ -232,6 +232,7 @@ return [
         'Validator'    => Illuminate\Support\Facades\Validator::class,
         'View'         => Illuminate\Support\Facades\View::class,
         'Excel'        => Maatwebsite\Excel\Facades\Excel::class,
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class
     ],
 
 ];
